57eb9af4706cc11d9f4fe5c2a25d11c1176e6663
saroj bata <lordhunter017@gmail.com>
2023-11-01 12:15:33 +0545

    changes

---
+++ Day_01/JS_Variables.js
@@ -0,0 +1,32 @@
+console.log ("Hello World");
+
+
+var person; /*Creating Veriable */
+person = {name: "John", age: 30, city: "New York"}; //Assigning Value to the Variable
+console.log(person); //Printing out the value of the variable 
+
+
+
+
+var getting = "hello"
+console.log(getting, person);
+
+
+// Coursera EXercise:Declaring VAriables
+var petdog = "Rex";
+var petCat = "Pepper";
+console.log(petdog);
+console.log(petCat);
+console.log("My Pet Dog's name is", petdog);
+console.log("My pet Cat name is", petCat);
+
+var catSound = "purr";
+var dogSound = "Woof"
+console.log(petdog, "says", dogSound);
+console.log(petCat, "says", catSound);
+
+
+
+
+var catSound = "meow";
+console.log(petCat, "now say", catSound);
