08761a878079bade42b16b7b31f789dc40e48b16
saroj bata <lordhunter017@gmail.com>
2023-11-01 13:07:46 +0545

    add more files

---
+++ Day_01/JS_Data_type.js
@@ -0,0 +1,30 @@
+// There Are 7 types of premative Data Types
+/* THey are:
+1. Number (integer, float)
+2. String
+3. Boolean
+4. Null
+5. Undefined
+6. BigInt
+7. Symbol
+*/
+
+
+// Declare variables for each type of primitive data type
+let num = 123;
+let str = 'Hello World';
+let bool = true;
+let nullVar = null;
+let undefinedVar = undefined;
+let bigInt = 12345678901234567890n;
+let symbol = Symbol('description');
+
+
+// Print each variable's data type
+console.log(typeof num); // "number"
+console.log(typeof str); // "string"
+console.log(typeof bool); // "boolean"
+console.log(typeof nullVar); // "object"
+console.log(typeof undefinedVar); // "undefined"
+console.log(typeof bigInt); // "bigint"
+console.log(typeof symbol); // "symbol"
---
+++ Day_01/JS_Operators.js
@@ -0,0 +1,20 @@
+// Assigment Operators
+let x = 10;
+let y = 12;
+console.log(x + y);
+console.log( x - y);
+console.log(x  / y);
+console.log(x * y);
+
+
+// Comparison Operators
+console.log(x > y);
+console.log(x < y);
+console.log( x == y);
+console.log( x != y);
+
+
+
+// Logical Operators
+console.log(x > y && x<y);
+console.log(x > y || x<y);
